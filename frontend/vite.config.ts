6c89b9f409a871db0fe6c62244b4e699ccc57041
Rory Preddy <rorypreddy@gmail.com>
2025-03-17 08:06:00 +0200

    Initialize frontend with Vite, Vue, and essential dependencies

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5073',
+        changeOrigin: true
+      }
+    }
+  }
+})
